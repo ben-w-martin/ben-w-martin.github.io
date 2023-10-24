d9fd3a32188e58f7c150b000daf0366fb10ef981
Benjamin Martin <ben.w.martin17@gmail.com>
2023-10-24 18:00:49 -0400

    var to let

--- js/portfolio.js
+++ js/portfolio.js
@@ -6,9 +6,9 @@ function dropMenu() {
 window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
         // Check if the clicked element or its parent is a .dropdown-container
-        var dropdowns = document.getElementsByClassName("dropdown-container");
-        for (var i = 0; i < dropdowns.length; i++) {
-            var dropdown = dropdowns[i];
+        let dropdowns = document.getElementsByClassName("dropdown-container");
+        for (let i = 0; i < dropdowns.length; i++) {
+            let dropdown = dropdowns[i];
             if (dropdown.classList.contains('show') &&
                 !dropdown.contains(event.target) &&
                 !event.target.matches('.dropdown-content')) {

24c448db64d777fe0d1b6209ab478f6d09a79e6f
Benjamin Martin <ben.w.martin17@gmail.com>
2023-10-26 16:06:13 -0400

    begin creating functions for react

---
+++ js/functions/arr-to-ul.js
@@ -0,0 +1,14 @@
+function arrToUl(arr) {
+    let arrAsUl = [];
+    for (let el of arr) {
+        el = "<li>" + el + "</li>"
+        arrAsUl.push(el);
+    }
+    arrAsUl.unshift("<ul>");
+    arrAsUl.push("</ul>");
+    arrAsUl = arrAsUl.join("");
+    return arrAsUl;
+}
+
+// console.log(arrToUl([1, 2, 3, 4, 5]));
+// console.log(arrToUl(["1", "2", "3", "4", "5"]));
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,21 +1,24 @@
 "use strict";
 
 function dropMenu() {
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("contact-dropdown").classList.toggle("show");
 }
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        // Check if the clicked element or its parent is a .dropdown-container
-        let dropdowns = document.getElementsByClassName("dropdown-container");
+
+window.onclick = function (event) {
+    if (!event.target.matches(".dropbtn")) {
+        let dropdowns = document.getElementsByClassName("dropdown-container")
         for (let i = 0; i < dropdowns.length; i++) {
             let dropdown = dropdowns[i];
-            if (dropdown.classList.contains('show') &&
-                !dropdown.contains(event.target) &&
-                !event.target.matches('.dropdown-content')) {
-                dropdown.classList.remove('show');
+            if (dropdown.classList.contains("show") &&
+                !event.target.matches("dropdown-items") &&
+                !dropdown.contains(event.target)) {
+                dropdown.classList.remove("show");
             }
         }
     }
-};
+}
 
+// function runCode(e) {
+//     e.preventDefault();
+// }
 

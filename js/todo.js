934539ffeff89b26018231092675148768fbc68a
Benjamin Martin <ben.w.martin17@gmail.com>
2023-11-01 15:22:02 -0400

    refactored so that currentTodos[] !includes html tags

--- js/todo.js
+++ js/todo.js
@@ -1,18 +1,39 @@
 "use strict";
+
 (() => {
-    let currentTodos = [];
+    const currentTodos = [];
     const currentTodosUl = document.querySelector(".current-todos-ul");
     const todoForm = document.getElementById("todo-form");
 
     todoForm.addEventListener("submit", function (e) {
         e.preventDefault();
-        const newTodo =  `<input type="checkbox"> ${document.getElementById("newTodo").value} <button id="delete"><img src="img/times-icon.svg" alt="Delete"></button>`;
+        let newTodo = document.getElementById("newTodo").value;
+        currentTodos.push(newTodo);
+        newTodo = `<input type="checkbox"> ${currentTodos[currentTodos.length - 1]} <button id="delete"><img src="img/times-icon.svg" alt="Delete"></button>`;
         const frag = document.createDocumentFragment();
         const li = document.createElement("li");
-        currentTodos.push(newTodo);
-        li.innerHTML = currentTodos[currentTodos.length - 1];
+        li.innerHTML = newTodo;
         frag.appendChild(li);
         currentTodosUl.appendChild(frag);
+    });
 
-    })
 })();
+
+
+// (() => {
+//     let currentTodos = [];
+//     const currentTodosUl = document.querySelector(".current-todos-ul");
+//     const todoForm = document.getElementById("todo-form");
+//
+//     todoForm.addEventListener("submit", function (e) {
+//         e.preventDefault();
+//         const newTodo =  `<input type="checkbox"> ${document.getElementById("newTodo").value} <button id="delete"><img src="img/times-icon.svg" alt="Delete"></button>`;
+//         const frag = document.createDocumentFragment();
+//         const li = document.createElement("li");
+//         currentTodos.push(newTodo);
+//         li.innerHTML = currentTodos[currentTodos.length - 1];
+//         frag.appendChild(li);
+//         currentTodosUl.appendChild(frag);
+//
+//     })
+// })();

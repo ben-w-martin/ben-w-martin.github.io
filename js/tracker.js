a4114abcee5a75983a2629a1f7b97e8f0f577ef2
Benjamin Martin <ben.w.martin17@gmail.com>
2023-11-29 23:54:46 -0500

    did some styling to distract me from js

--- js/tracker.js
+++ js/tracker.js
@@ -1,5 +1,14 @@
 "use strict";
 
 (() => {
-    console.log("hello world");
+    const exerciseSelect = document.querySelector("#exercises");
+
+    fetch("../data/exercises.json").then(response => response.json()).then(data => {
+        console.log(data);
+        data[0].exercise.forEach(item => {
+            const option = document.createElement("option");
+            option.innerText = item.name;
+            exerciseSelect.appendChild(option);
+        });
+    });
 })();

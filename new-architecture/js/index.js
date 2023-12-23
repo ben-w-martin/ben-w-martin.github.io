8221f5be63ccc525cff18c2bbb3da0149300282c
Benjamin Martin <ben.w.martin17@gmail.com>
2023-12-23 12:09:45 -0500

    header-box bg color

--- new-architecture/js/index.js
+++ new-architecture/js/index.js
@@ -3,6 +3,7 @@
 // ABOUT TAB SWITCHER
 const tabs = document.querySelectorAll(".tabs__input");
 const infoBlocks = document.querySelectorAll(".info__box");
+const headerBoxes = document.querySelectorAll(".tabs__header-box");
 
 // EVENT LISTENERS
 for (let i = 0; i < tabs.length; i++) {
@@ -10,6 +11,7 @@ for (let i = 0; i < tabs.length; i++) {
         infoBlocks.forEach(block => {
             block.style.display = "none";
         })
+        // headerBoxes[i].style.backgroundColor = "$color-grey-a-dk";
         infoBlocks[i].style.display = "block";
     });
 }

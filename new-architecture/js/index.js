5a85cdc1414af8b7c207ee289b91e0710d498b2b
Benjamin Martin <ben.w.martin17@gmail.com>
2023-12-22 19:51:47 -0500

    refactor class labels

--- new-architecture/js/index.js
+++ new-architecture/js/index.js
@@ -1,3 +1,15 @@
 "use strict";
 
 // ABOUT TAB SWITCHER
+const tabs = document.querySelectorAll(".about__input");
+const infoBlocks = document.querySelectorAll(".about__tab");
+
+// EVENT LISTENERS
+for (let i = 0; i < tabs.length; i++) {
+    tabs[i].addEventListener("change", function () {
+        infoBlocks.forEach(block => {
+            block.style.display = "none";
+        })
+        infoBlocks[i].style.display = "block";
+    });
+}

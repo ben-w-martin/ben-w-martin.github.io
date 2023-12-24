2fa526cf39e991ff70113597834bb575d64811cf
Benjamin Martin <ben.w.martin17@gmail.com>
2023-12-24 13:46:56 -0500

    projects section

--- js/index.js
+++ js/index.js
@@ -7,15 +7,28 @@ const infoBlocks = document.querySelectorAll(".info__box");
 const headerBoxes = document.querySelectorAll(".tabs__header-box");
 const projBtn = document.querySelector(".btn__proj");
 const projBtn2 = document.querySelector(".btn__proj2");
+const backBtn = document.querySelector(".btn__back");
 
 // FUNCTIONS
 function projListener(e) {
     e.preventDefault(e);
-    const about = document.querySelector(".about");
+    const tabs = document.querySelector(".tabs");
+    const info = document.querySelector(".info");
     const projects = document.querySelector(".projects");
-    about.style.display = "none";
+    tabs.style.display = "none";
+    info.style.display = "none";
     projects.style.display = "block";
     // renderProjects();
+}
+
+function backListener(e) {
+    e.preventDefault();
+    const tabs = document.querySelector(".tabs");
+    const info = document.querySelector(".info");
+    const projects = document.querySelector(".projects");
+    projects.style.display = "none";
+    tabs.style.display = "flex";
+    info.style.display = "block";
 }
 
 // EVENT LISTENERS
@@ -34,3 +47,4 @@ for (let i = 0; i < tabs.length; i++) {
 
 projBtn.addEventListener("click", projListener);
 projBtn2.addEventListener("click", projListener);
+backBtn.addEventListener("click", backListener);

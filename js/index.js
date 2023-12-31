b99ec167d3f69d4db87af25b58ab614a3b411e2c
Benjamin Martin <ben.w.martin17@gmail.com>
2023-12-31 13:55:57 -0500

    add: scroll to projects

--- js/index.js
+++ js/index.js
@@ -1,50 +1,60 @@
 "use strict";
 
+(() => {
+
 // CONSTANTS
-const tabs = document.querySelectorAll(".tabs__input");
-const infoBlocks = document.querySelectorAll(".info__box");
-const headerBoxes = document.querySelectorAll(".tabs__header-box");
-const exampleImg = document.querySelectorAll(".example-img");
-const projBtn = document.querySelector(".btn__proj");
-const projBtn2 = document.querySelector(".btn__proj2");
-const backBtn = document.querySelector(".btn__back");
-const modalBg = document.querySelector(".modal");
+    const tabs = document.querySelectorAll(".tabs__input");
+    const infoBlocks = document.querySelectorAll(".info__box");
+    const headerBoxes = document.querySelectorAll(".tabs__header-box");
+    const projBtn = document.querySelector(".btn__proj");
+    const projBtn2 = document.querySelector(".btn__proj2");
+    const backBtn = document.querySelector(".btn__back");
+    const main = document.querySelector("#main");
 
 // FUNCTIONS
-function projListener(e) { // Directs to project "page"
-    e.preventDefault(e);
-    const tabs = document.querySelector(".tabs");
-    const info = document.querySelector(".info");
-    const projects = document.querySelector(".projects");
-    tabs.style.display = "none";
-    info.style.display = "none";
-    projects.style.display = "block";
-}
+    function projListener(e) { // Directs to project "page"
+        e.preventDefault();
 
-function backListener(e) { // Directs to home
-    e.preventDefault();
-    const tabs = document.querySelector(".tabs");
-    const info = document.querySelector(".info");
-    const projects = document.querySelector(".projects");
-    projects.style.display = "none";
-    tabs.style.display = "flex";
-    info.style.display = "block";
-}
+        const tabs = document.querySelector(".tabs");
+        const info = document.querySelector(".info");
+        const projects = document.querySelector(".projects");
+
+        window.scrollTo({
+            top: projects,
+            behavior: "smooth"
+        })
+
+        tabs.style.display = "none";
+        info.style.display = "none";
+        projects.style.display = "block";
+    }
+
+    function backListener(e) { // Directs to home
+        e.preventDefault();
+        const tabs = document.querySelector(".tabs");
+        const info = document.querySelector(".info");
+        const projects = document.querySelector(".projects");
+        projects.style.display = "none";
+        tabs.style.display = "flex";
+        info.style.display = "block";
+    }
 
 // EVENT LISTENERS
-for (let i = 0; i < tabs.length; i++) { // Adds Ev. listeners to tabs
-    tabs[i].addEventListener("change", function () {
-        infoBlocks.forEach(block => {
-            block.style.display = "none";
-        })
-        headerBoxes.forEach(box => {
-            box.style.backgroundColor = "transparent";
-        })
-        headerBoxes[i].style.backgroundColor = "rgba(98, 98, 98, 0.2)";
-        infoBlocks[i].style.display = "block";
-    });
-}
+    for (let i = 0; i < tabs.length; i++) { // Adds Ev. listeners to tabs
+        tabs[i].addEventListener("change", function () {
+            infoBlocks.forEach(block => {
+                block.style.display = "none";
+            })
+            headerBoxes.forEach(box => {
+                box.style.backgroundColor = "transparent";
+            })
+            headerBoxes[i].style.backgroundColor = "rgba(98, 98, 98, 0.2)";
+            infoBlocks[i].style.display = "block";
+        });
+    }
 
-projBtn.addEventListener("click", projListener);
-projBtn2.addEventListener("click", projListener);
-backBtn.addEventListener("click", backListener);
+    projBtn.addEventListener("click", projListener);
+    projBtn2.addEventListener("click", projListener);
+    backBtn.addEventListener("click", backListener);
+
+})();

92aa59b3c1aff03089f3f7c172a4e76bb4cb3071
Benjamin Martin <ben.w.martin17@gmail.com>
2024-05-11 13:41:31 -0400

    fix: build error: vite external dependencies

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  build: {
+    rollupOptions: {
+      external: [
+        "@fortawesome/react-fontawesome",
+        "@fortawesome/free-solid-svg-icons",
+      ],
+    },
+  },
 });

adf63b42a7a692921f2f0793346cc94115906a85
Benjamin Martin <ben.w.martin17@gmail.com>
2024-05-11 14:05:36 -0400

    fix: img folder added to public dir

--- src/components/experience_dropdown/DropdownItem.jsx
+++ src/components/experience_dropdown/DropdownItem.jsx
@@ -13,7 +13,7 @@ function DropdownItem({item, index}) {
           <div className="exp__img--right">
             <img
               className="exp__img-container--img"
-              src={`src/assets/img/${item.file}`}
+              src={`/public/img/${item.file}`}
               alt={"figure-" + index}
             />
           </div>

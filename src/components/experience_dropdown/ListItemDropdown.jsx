eb5d396dc3937c82e9053f7c1b105cb387ba1edb
Benjamin Martin <ben.w.martin17@gmail.com>
2024-05-11 11:49:05 -0400

    merge conflict

---
+++ src/components/experience_dropdown/ListItemDropdown.jsx
@@ -0,0 +1,35 @@
+import * as PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+import { DropdownItem } from "./DropdownItem";
+
+function ListItemDropdown({ options }) {
+  const [items, setItems] = useState({
+    array: [],
+    components: [],
+  });
+
+
+  useEffect(() => {
+    if (options && options?.length > 0) {
+      setItems(() => {
+        const newItems = {
+          array: options,
+          components: options.map(mapOptions),
+        };
+        return newItems;
+      });
+    }
+  }, [options]);
+
+  const mapOptions = (item, i) => {
+    console.log(item.string);
+    return <DropdownItem item={item} index={i}/>
+  };
+  return <>{items.components}</>;
+}
+
+ListItemDropdown.propTypes = {
+  options: PropTypes.array,
+};
+
+export { ListItemDropdown };

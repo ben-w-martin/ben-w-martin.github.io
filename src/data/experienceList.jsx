ae7d7ccb82cd3062de4cb792fcfbb2bb929daaae
Benjamin Martin <ben.w.martin17@gmail.com>
2024-06-12 23:21:20 -0400

    updated resume

--- src/data/experienceList.jsx
+++ src/data/experienceList.jsx
@@ -2,86 +2,76 @@ import { ListItemDropdown } from "../components/experience_dropdown/ListItemDrop
 import * as options from "./listItemOptions";
 
 const experienceList = [
+    [
+      {
+          "string": "Migrated sensitive processes away from the application level by developing event-driven Cloud Functions in Firebase, optimizing system performance and enabling seamless expansion to support future growth and increasing demands.",
+          "subcomponent": null
+      },
+      {
+          "string": "Developed in a virtual Linux environment accessed over SSH via Google Workstations.",
+          "subcomponent": null
+      }
+  ],
+    [
+      {
+          "string": "Increased development speed by 35% by generating forms using available third-party libraries like Formik and Yup.",
+          "subcomponent": <ListItemDropdown key="dropdown-Forms" options={options.tabiVenues} />
+      },
+      {
+          "string": "Enriched user experience by creating advanced features and responsive designs using CSS and Bootstrap.",
+          "subcomponent": null
+      },
+      {
+          "string": "Increased front-end performance by 50% by leveraging data structures and algorithms to improve functions.",
+          "subcomponent": null
+      },
+      {
+          "string": "Mitigated financial vulnerabilities, decreased risk of malicious requests, and protected user data by designing RESTful Web APIs in .NET Core with multilayered request validation and request models with stringent data annotations.",
+          "subcomponent": null
+      },
+      {
+          "string": "Ensured confidentiality, integrity, and availability of information in relational database development by engineering normalized tables, stored procedures, and user-defined functions using SQL Server and T-SQL.",
+          "subcomponent": null
+      },
+      {
+          "string": "Employed SOLID principles and C# design patterns in back-end architecture by developing ASP.NET API controllers that implement ADO.NET data-access interfaces with classes instantiated as singletons in dependency injection.",
+          "subcomponent": null
+      },
+      {
+          "string": "Restricted access to sensitive site features with authorization, authentication, and roles-based security policies.",
+          "subcomponent": null
+      },
+      {
+          "string": "Contributed to workflow in Agile environment through daily stand-ups, code reviews, and code talks.",
+          "subcomponent": null
+      },
+      {
+          "string": "Created staging and production-ready code by emphasizing accountability in the version control and hosting process with Git and GitHub, decreasing production bugs by 75%.",
+          "subcomponent": null
+      }
+  ],
   [
     {
-      string:
-        "Created scalable Cloud Functions in Firebase, optimizing system performance and enabling seamless expansion to support future growth and increasing user demands.",
-      subcomponent: null,
+        "string": "Scored in the 98th percentile on the Armed Services Vocational Aptitude Battery (ASVAB).",
+        "subcomponent": null
+    },
+    {
+        "string": "Used electronic healthcare systems to maintain HIPAA compliance on overseas deployment, ensuring privacy and quality treatment for 500 soldiers, resulting in Army Commendation and Army Achievement Medals.",
+        "subcomponent": null
     }
-  ],
-  [
-    {
-      string:
-        "Engineered robust database structures, tables, stored procedures, and functions using SQL.",
-      subcomponent: null,
-    },
-    {
-      string:
-        "Developed RESTful APIs with .NET Core to ensure secure HTTP request routing between client and server.",
-      subcomponent: null,
-    },
-    {
-      string:
-        "Created domain and data-annotated request models in ASP.NET for reliable binding and universal type-accuracy.",
-      subcomponent: null,
-    },
-    {
-      string:
-        "Implemented third party component libraries such as Formik, Yup, and React Google Maps to create powerful User Interfaces with type-safe input capabilities.",
-      subcomponent: (
-        <ListItemDropdown key="dropdown-Forms" options={options.tabiVenues} />
-      ),
-    },
-    {
-      string:
-        "Contributed to Agile environment through daily stand-ups, code reviews, and code talks.",
-      subcomponent: null,
-    },
-    {
-      string:
-        "Collaborated on production efforts through Git and GitHub version control services, merge conflict resolution, and clear communication with senior developers.",
-      subcomponent: null,
-    },
-    {
-      string:
-        "Provided meaningful feedback on Pull Requests to improve upon adherence to development standards and industry best practices.",
-      subcomponent: null,
-    },
-    {
-      string:
-        "Produced roles-based user profiles with tailored access and content management capabilities.",
-      subcomponent: null,
-    },
-    {
-      string:
-        "Leveraged data structures and algorithms to conform API response objects to custom formats, creating a richer functionality and user experience.",
-      subcomponent: null,
-    },
-  ],
-  [
-    {
-      string:
-        "Scored in the 98th percentile on the Armed Services Vocational Aptitude Battery (ASVAB).",
-      subcomponent: null,
-    },
-    {
-      string:
-        "Used electronic healthcare systems to maintain HIPAA compliance on overseas deployment, ensuring confidentiality, integrity, and availability of patient information, resulting in Army Commendation and Army Achievement Medals.",
-      subcomponent: null,
-    },
-  ],
-  [
-    {
-      string:
-        "Developed web application that leverages APIs to plan stargazing trips using real-time space data.",
-      subcomponent: <ListItemDropdown options={options.spaceProject} />,
-    },
-    {
-      string:
-        "Used mathematical formulas in code to leverage geographical data to create smart features.",
-      subcomponent: null,
-    }
-  ]
+],
+
+[
+  {
+      "string": "Developed web application that accesses APIs to plan stargazing trips using real-time space data - Repository",
+      "subcomponent": <ListItemDropdown options={options.spaceProject} />
+  },
+  {
+      "string": "Translated mathematical formulas into JavaScript to elicit smart features from raw geographical data.",
+      "subcomponent": null
+  }
+]
+
 ];
 
 export default experienceList;
